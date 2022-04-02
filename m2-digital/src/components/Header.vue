6cdd9f8a14f28f06e8410e25b239fce4762fbfc1
Felipe Oliveira <oliveira.felipe28@gmail.com>
2022-04-01 23:29:04 -0300

    vue skeleton commit

---
+++ m2-digital/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="header-div">
+        <h1> TV FRONT </h1>
+        <p>&#128722</p>
+
+    </div>
+
+    
+</template>
+
+
+
+
+<script>
+export default {
+    data(){
+        return {
+            'name': 'Header'
+        }
+    }
+}
+</script>
+
+
+<style>
+    .header-div{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100%;
+        height: 10vh;
+        background-color: rgba(182, 84, 35, 0.594);
+        padding: 30px 70px;
+    }
+
+    h1{
+        font-size: 75px;
+        padding: 40px 0 0;
+        color: rgb(15, 13, 13);
+    }
+
+    p{
+        font-size: 90px;
+    }
+
+</style>
